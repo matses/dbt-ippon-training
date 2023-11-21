3e7467516717631bf665b887820834ac77eec0fe
msescosse@ippon.fr <msescosse@ippon.fr>
2023-11-21 09:39:56 +0000

    dqss

--- macros/default__generate_schema_name.sql
+++ macros/default__generate_schema_name.sql
@@ -1,8 +1,10 @@
 {% macro default__generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none || target.name == 'prod' -%}
+    {%- if custom_schema_name is none -%}
         {{ default_schema }}
+    {% elif target.name == 'prod'%}
+        {{ custom_schema_name | trim }}
     {%- else -%}
         {{ default_schema }}_{{ custom_schema_name | trim }}
     {%- endif -%}
