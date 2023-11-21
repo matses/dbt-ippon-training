8628b4b77d3409ccd7083f31e329943b93e31d8b
msescosse@ippon.fr <msescosse@ippon.fr>
2023-11-21 09:33:49 +0000

    jhcdsjkhbsdf

---
+++ macros/default__generate_schema_name.sql
@@ -0,0 +1,10 @@
+{% macro default__generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none || target.name == 'prod' -%}
+        {{ default_schema }}
+    {%- else -%}
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+    {%- endif -%}
+
+{%- endmacro %}
